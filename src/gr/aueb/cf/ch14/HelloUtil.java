14d9d40734961929aff8df03dd263fcb35d56038
G. Dalezios <georgedalezios@hotmail.com>
2024-06-03 21:06:30 +0300

    -m Updates

---
+++ src/gr/aueb/cf/ch14/HelloUtil.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.ch14;
+
+public class HelloUtil {
+
+    /**
+     * No instances of this class should
+     * be available.
+     */
+    private HelloUtil() {
+
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello World!");
+    }
+}
+
+
---
+++ src/gr/aueb/cf/ch14/PointMain.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.ch14;
+
+public class PointMain {
+
+    public static void main(String[] args) {
+        Point zeroPoint = Point.getZeroPoint();
+        Point randomPoint = Point.getRandomPoint();
+    }
+}
