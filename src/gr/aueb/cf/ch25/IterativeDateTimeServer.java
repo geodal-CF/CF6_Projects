5f449165884d84eff07094493c842bcd2d7418ed
G. Dalezios <georgedalezios@hotmail.com>
2024-08-03 16:47:59 +0300

    iterative thread starts

---
+++ src/gr/aueb/cf/ch25/IterativeDateTimeServer.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.ch25;
+
+public class IterativeDateTimeServer {
+
+    public static void main(String[] args) {
+        DateTimeServer dateTimeServer = new DateTimeServer();
+        dateTimeServer.start();
+    }
+}
